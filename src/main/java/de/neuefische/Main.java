d320faa74493c92366cb76ec2b82e23dc982fbf1
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-29 16:13:59 +0200

    Create instances of Medication and put them in Pharmacy instance

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -8,10 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Pharmacy myPharmacy = new Pharmacy();
 
-        System.out.println(myPharmacy.getCount());
-
         myPharmacy.save(new Medication("test",9.99, true));
+        myPharmacy.save(new Medication("test2",19.99, true));
+        myPharmacy.save(new Medication("test3",29.99, true));
 
-        System.out.println(myPharmacy.getCount());
+        System.out.println(myPharmacy.find("test3"));
     }
 }
